585e9bdfd554eabfcbc95539db05131dc1b25ad7
patelhetal07 <84508817+patelhetal07@users.noreply.github.com>
2022-12-27 17:51:46 +0530

    set success page

--- application/views/success.php
+++ application/views/success.php
@@ -1,31 +1,100 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>xDreamPay : Success</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+?>
+<!DOCTYPE html>
+<html>
 
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Henexopay : Success</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
 </head>
+<style>
+	#header {
+		transition: all 0.5s;
+		z-index: 997;
+		padding: 20px 0;
+	}
+	.fixed-top {
+		position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		z-index: 1030;
+	}
+	#xpay {
+		width: 100%;
+		height: 100vh;
+		background: url(<?=base_url("assets/image/payment.png")?>) top center;
+		background-size: cover;
+		position: relative;
+	}
+	section {
+		padding: 60px 0;
+		overflow: hidden;
+	}
+	#xpay {
+		background-attachment: fixed;
+	}
+	#xpay .xpay-container {
+		position: absolute;
+		bottom: 0;
+		top: 0;
+		left: 0;
+		right: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+		text-align: center;
+		padding: 0 15px;
+	}
+	#xpay h3 {
+
+		font-size: 26px;
+		padding: 10px 30px;
+		margin-bottom: 30px;
+		border-radius: 50px;
+	}
+	#xpay:before {
+		content: "";
+		background: rgba(0, 0, 0, 0.6);
+		position: absolute;
+		bottom: 0;
+		top: 0;
+		left: 0;
+		right: 0;
+	}
+	.card {
+		border-radius: 8px !important;
+	}
+</style>
 <body>
 
-<div class="container mt-3">
-	<h2 class="text-center">xDreamPay</h2>
-	<div class="row mt-2">
-		<div class="col align-self-start">
-
-		</div>
-		<div class="col align-self-center">
-			<div class="alert alert-success p-3" role="alert">
-				Payment transaction completed successfully.
+<header id="header" class="fixed-top">
+	<div class="container d-flex align-items-center justify-content-between">
+		<h1 class="logo">
+			<img alt="Henexopay" src="<?=base_url("assets/image/logo.png")?>" width="214">
+		</h1>
+	</div>
+</header>
+<section id="xpay">
+	<div class="xpay-container">
+		<div class="card">
+			<div class="card-body">
+				<h3 class="mb-0 pb-0">Payment transaction completed successfully.</h3>
+				<h6 class="mb-4">Please check your transaction details</h6>
+				<?php echo '<pre>';
+				print_r($_REQUEST);
+				?>
 			</div>
 		</div>
-		<div class="col align-self-end">
-
-		</div>
 	</div>
-</div>
-
+</section>
 </body>
+
 </html>
+
+
+
